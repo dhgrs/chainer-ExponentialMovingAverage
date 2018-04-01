7e89036973a194ad702ef6da73bdc8f1d1d94250
dhgrs <d.higurashi@gmail.com>
2018-04-01 17:47:13 +0900

    fix a bug with scope

--- exponential_moving_average.py
+++ exponential_moving_average.py
@@ -11,21 +11,24 @@ class ExponentialMovingAverage(link.Chain):
         with self.init_scope():
             self.target = target
             self.ema = target.copy()
-            for name, param in target.namedparams():
-                setattr(self.ema, name, chainer.Parameter(param.array))
+            with self.ema.init_scope():
+                for name, param in target.namedparams():
+                    name = name[1:]  # remove prefix
+                    setattr(self.ema, name, chainer.Parameter(param.array))
 
     def __call__(self, *args, **kwargs):
         if configuration.config.train:
             ys = self.target(*args, **kwargs)
-            with self.init_scope():
-                for name, target_param in self.target.namedparams():
-                    ema_param = getattr(self.ema, name)
-                    if (not target_param.requires_grad
-                            or ema_param.array is None):
-                        new_average = target_param.array
-                    else:
-                        new_average = self.decay * target_param.array + \
-                            (1 - self.decay) * ema_param.array
+            for name, target_param in self.target.namedparams():
+                name = name[1:]  # remove prefix
+                ema_param = getattr(self.ema, name)
+                if (not target_param.requires_grad
+                        or ema_param.array is None):
+                    new_average = target_param.array
+                else:
+                    new_average = self.decay * target_param.array + \
+                        (1 - self.decay) * ema_param.array
+                with self.ema.init_scope():
                     setattr(self.ema, name, chainer.Parameter(new_average))
         else:
             ys = self.ema(*args, **kwargs)
